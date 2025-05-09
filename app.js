8412c14044e3bf317a93328bdc7206acc3e4e9ca
saxoncuddie <saxonlee2000@outlook.com>
2025-05-09 11:50:33 -0400

    app.js fix AGAIN

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ const limiter = rateLimit({
 });
 app.use(limiter);
 
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
 
 db.connect((err) => {
     if (err) {
