0f27f075edb5a2412bada2c5b25e65a563988b8d
saxoncuddie <saxonlee2000@outlook.com>
2025-05-09 11:29:57 -0400

    app.js update

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
-// app.js (Backend Entry Point)
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const path = require('path');
 require('dotenv').config();
+const mysql = require('mysql2');
 
 const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+    origin: 'grctrackfrontend-a0g0csghatcehffq.centralus-01.azurewebsites.net', 
+    credentials: true
+}));
 app.use(express.json());
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minute rate limit
-    max: 100,
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100
 });
 app.use(limiter);
+
+
+db.connect((err) => {
+    if (err) {
+        console.error('Database connection failed:', err.message);
+    } else {
+        console.log('Connected to MySQL database.');
+    }
+});
+
 
 app.use('/api/auth', require('./Routes/auth'));
 app.use('/api/logs', require('./Routes/logs'));
